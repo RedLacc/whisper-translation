9de52c1fc70165888de7dfea9ef428fc522b534e
jack <skiplacc@gmail.com>
2025-07-13 22:42:46 +0800

    新增-ta参数

--- main.go
+++ main.go
@@ -11,34 +11,44 @@ import (
 	"path"
 )
 
+// -tf=FC2-PPV-4189226-14.mp4 -tcs=2 -tbs=3 -tom=qwen3:14b -- -m "D:\whisper.cpp\models\ggml-large-v3-turbo.bin" -l ja -osrt whisper-out
+
 func main() {
 	run()
 }
 
 var (
-	contextSize = flag.Int("tcs", 2, "context size")
-	batchSize   = flag.Int("tbs", 1, "batch size")
-	ollamaModel = flag.String("tom", "", "ollama model name")
-	message     = flag.String("tm", "", "a message that describe the content of transcribed text to make better translation")
-	filePath    = flag.String("tf", "", "file path")
+	contextSize   = flag.Int("tcs", 2, "context size")
+	batchSize     = flag.Int("tbs", 1, "batch size")
+	ollamaModel   = flag.String("tom", "", "ollama model name")
+	message       = flag.String("tm", "", "a message that describe the content of transcribed text to make better translation")
+	filePath      = flag.String("tf", "", "file path")
+	audioFilePath = flag.String("ta", "", "file path")
 )
 
 func run() {
 	flag.Parse()
 	args := flag.Args()
-
-	ffmpegCmd := exec.Command("ffmpeg", "-i", *filePath, "-vn", "-acodec", "pcm_s16le", "-ar", "44100", "-ac", "1", "output.wav")
-	ffmpegCmd.Stderr = os.Stderr
-	ffmpegCmd.Stdout = os.Stdout
-	err := ffmpegCmd.Run()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+	audioFileName := ""
+	if *audioFilePath == "" {
+		audioFileName = "output.wav"
+		ffmpegCmd := exec.Command("ffmpeg", "-i", *filePath, "-vn", "-acodec", "pcm_s16le", "-ar", "44100", "-ac", "1", audioFileName)
+		ffmpegCmd.Stderr = os.Stderr
+		ffmpegCmd.Stdout = os.Stdout
+		err := ffmpegCmd.Run()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		defer os.Remove(audioFileName)
+	} else {
+		audioFileName = *audioFilePath
 	}
 
 	translater := NewTranslate(*message, *ollamaModel, *contextSize, *batchSize)
 
-	whisperCmd := exec.Command("whisper-cli", append(args, "-f", "output.wav")...)
+	whisperCmd := exec.Command("whisper-cli", append(args, "-f", audioFileName)...)
+	whisperCmd.Stderr = os.Stderr
 
 	whisperOut, err := whisperCmd.StdoutPipe()
 	if err != nil {
@@ -70,21 +80,6 @@ func run() {
 		}
 		close(done)
 	}()
-
-	whisperErr, err := whisperCmd.StderrPipe()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	defer whisperErr.Close()
-
-	go func() {
-		scanner := bufio.NewScanner(whisperErr)
-		for scanner.Scan() {
-			fmt.Fprintf(os.Stderr, "%s\n", scanner.Text())
-		}
-	}()
-
 	err = whisperCmd.Run()
 	if err != nil {
 		fmt.Println(err)
@@ -101,6 +96,9 @@ func run() {
 }
 
 func WriteSrt(fileName string, lines []*Line) {
+	if len(lines) == 0 {
+		return
+	}
 	buf := bytes.NewBuffer(make([]byte, 0, len(lines)*len(lines[0].Original)))
 	for i, line := range lines {
 		fmt.Fprintf(buf, "%d\n", i+1)
